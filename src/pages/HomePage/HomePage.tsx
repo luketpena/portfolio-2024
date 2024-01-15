746aa2012560d8f7c42c2989b4a0abc045d7d4b8
Luke <luketpena@gmail.com>
2024-01-15 13:26:19 -0600

    chore: implement page wrapper on home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,9 @@
+import { PageWrapper } from '../../components/PageWrapper/PageWrapper';
+
 export const HomePage: React.FC = () => {
   return (
-    <div>
+    <PageWrapper>
       <h1 className="text-red-500 bg-red-500">What's up</h1>
-    </div>
+    </PageWrapper>
   );
 };
