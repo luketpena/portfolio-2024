7b37bcaf8ca2a5810ea5ce17112a58af519db1eb
Luke <luketpena@gmail.com>
2024-01-15 13:30:15 -0600

    chore: remove image from highlight

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import { ReactNode } from 'react';
 import Icon, { IconName } from '../Icon/Icon';
 
@@ -6,7 +5,6 @@ interface HighlightProps {
   title?: string;
   subtitle?: string;
   icon?: IconName;
-  img?: string;
   children?: ReactNode;
   unfocusImg?: boolean;
   color: string;
@@ -17,36 +15,10 @@ export const Highlight: React.FC<HighlightProps> = ({
   title,
   subtitle,
   icon,
-  img,
   children,
-  unfocusImg,
 }) => {
   return (
-    <div
-      className={classNames('-mx-4 grid shadow-lg relative', {
-        'grid-cols-[1fr_3fr]': !!img,
-      })}
-    >
-      {img && (
-        <div className="w-full h-full overflow-hidden">
-          <div
-            className={classNames('bg-cover relative w-full h-full', {
-              'brightness-50 scale-125 blur-sm': unfocusImg,
-            })}
-            style={{
-              backgroundImage: `url("${img}")`,
-            }}
-          >
-            {unfocusImg && (
-              <div
-                className="absolute left-0 top-0 w-full h-full mix-blend-color"
-                style={{ backgroundColor: color }}
-              />
-            )}
-          </div>
-        </div>
-      )}
-
+    <div className="-mx-4 grid shadow-lg relative">
       <div className="bg-slate-700 p-4">
         <div className="flex items-center gap-1 md:gap-4 flex-col md:flex-row">
           <div className="flex items-center gap-2">
